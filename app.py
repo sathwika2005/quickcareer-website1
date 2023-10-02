20464c6c9c3b552cc3cb30856181f04ad453f84f
sathwika2005 <118988063+sathwika2005@users.noreply.github.com>
2023-10-02 06:16:07 +0000

    flask application

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return "Hello World!!"
+@app.route("/home")
+def home_page():
+  return "Home Page"
+
+@app.route("/about")
+def about_page():
+  return "About Page"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
