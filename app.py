e1134bb80c512dacffb5567979eedafec92b59bd
sathwika2005 <118988063+sathwika2005@users.noreply.github.com>
2023-10-06 07:33:52 +0000

    added  HTML and CSS

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
 def home_page():
-  return "Home Page"
-
-@app.route("/about")
-def about_page():
-  return "About Page"
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
